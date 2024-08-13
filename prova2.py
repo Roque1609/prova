f92fafc2538f3c4d3a2b57fe7741a073a0e6e6c6
Roque <60229903+Roque1609@users.noreply.github.com>
2024-08-13 03:31:57 -0300

    prova2

---
+++ prova2.py
@@ -0,0 +1,10 @@
+# Prova 2
+
+num = float(input("Digite um número: "))
+
+if num > 0:
+    print(f"Seu número é positivo.")
+elif num < 0:
+    print(f"Seu número é negativo.")
+else:
+    print(f"Seu número é zero.")
